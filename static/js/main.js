048383e3cc0ad44f8f536f0fd48bd3f232d1d4bd
Russ Biggs <russbiggs@gmail.com>
2019-09-07 13:21:42 -0600

    menu, toc and article build out

---
+++ static/js/main.js
@@ -0,0 +1,78 @@
+(function() {
+    browseDocs();
+    accordionExpand();
+    var languageSelect = document.querySelector('.js-language-select');
+    var languageSelectOptions = document.querySelector('.js-language-select-options');
+    languageSelect.open = false;
+    languageSelect.addEventListener('click', function(e) {
+        if (!languageSelect.open) {
+            languageSelect.open = true;
+            languageSelectOptions.classList.remove('language-select-options--hidden');
+        } else {
+            languageSelect.open = false;
+            languageSelectOptions.classList.add('language-select-options--hidden');
+        }
+    });
+
+    var languageOptions = languageSelectOptions.children;
+    for (var i =0; i< languageOptions.length; i++) {
+        languageOptions[i].addEventListener('click', languageClick)
+    }
+
+    function languageClick(e) {
+        var selectedLanguage = document.querySelector('.language-select-option--selected');
+        if (e.target != selectedLanguage) {
+            selectedLanguage.classList.remove('language-select-option--selected');
+            e.target.classList.add('language-select-option--selected');
+            languageSelect.innerHTML = e.target.innerHTML;
+            languageSelect.open = false;
+            languageSelectOptions.classList.add('language-select-options--hidden');
+        }
+    }
+
+
+
+})();
+
+function browseDocs() {
+    var browseSelect = document.querySelector('.js-browse-select');
+    var browseArrow = document.querySelector('.js-browse-select__arrow');
+    var docsOptions = document.querySelector('.js-browse-select-options');
+    browseSelect.open = false;
+    browseSelect.addEventListener('click', function(e) {
+        var classRemove = e.target.open ? 'browse-select__arrow' : 'browse-select__arrow--rotated'; 
+        var classAdd = e.target.open ? 'browse-select__arrow--rotated' : 'browse-select__arrow'; 
+        browseArrow.classList.remove(classRemove);
+        browseArrow.classList.add(classAdd);
+
+        if (!e.target.open) {
+            docsOptions.classList.remove("browse-select-options--hidden")
+        } else {
+            docsOptions.classList.add("browse-select-options--hidden")
+        }
+        e.target.open = e.target.open ? false : true;
+    });
+}
+
+function accordionExpand() {
+    var accordions = document.querySelectorAll('.accordion');
+    for (var i = 0; i<accordions.length; i++) {
+        accordions[i].open = false;
+        accordions[i].addEventListener('click', accordionListener);
+    }
+}
+
+function accordionListener(e) {
+    var accordion = e.target;
+    var panel = accordion.nextElementSibling;
+    if (!accordion.open) {
+        accordion.classList.remove('accordion--closed');
+        accordion.classList.add('accordion--open');
+        panel.classList.remove('panel--closed');
+    } else {
+        accordion.classList.add('accordion--closed');
+        accordion.classList.remove('accordion--open');
+        panel.classList.add('panel--closed');
+    }
+    accordion.open = accordion.open ? false : true;
+}
